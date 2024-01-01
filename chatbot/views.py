4410df0eb52b3468a0509a7b406ceaf3baaf7318
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-01 20:53:19 +0530

    Modified views.py line: 50 ==> changed retreiving method from dot to get

--- chatbot/views.py
+++ chatbot/views.py
@@ -47,7 +47,7 @@ def combine_documents(docs):
     return '\n'.join(doc['pageContent'] for doc in docs)
 
 def retrieve_context(resp):
-    return resp.standalone_question
+    return resp.get("standalone_question")
 
 
 answerTemplate = '''You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend. Keep the conversations short to medium.
