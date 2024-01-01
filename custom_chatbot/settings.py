7c507ea098ccd7a7ce92fdebf699d95498d089d8
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-01 20:19:56 +0530

    Added VM public IP as known address in settings.py

--- custom_chatbot/settings.py
+++ custom_chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ww9&5=$5)e5alj5yh9&g42lly7b)he3!=wkcqa+k)9a6ab(b(d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.172.154.123"]
 
 
 # Application definition
