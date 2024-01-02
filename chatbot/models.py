6450658a107fc1c9f9bfd1ae2b16abb2d0ed180c
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-02 22:12:13 +0530

    Storing chats in sqlite

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
 class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     response = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     unique_id = models.TextField()
 
     def __str__(self):
-        return f'{self.user.username}: {self.message}'
+        return f'{self.created_at}: {self.message}'
