9dd88dc991efc99121ec42b4d6cf25d17f7ca876
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-02 17:32:22 +0530

    Added async and await

--- chatbot/views.py
+++ chatbot/views.py
@@ -57,7 +57,7 @@ based on the context provided. Try to find the answer in the context.
 Encourage a casual conversation if the used tells about his name or ask about yourself, 
 like a friendly conversation.
 But the priority is to find the answer form the context. If you really don't know the answer, say 
-"I'm sorry, I don't know the answer to that." and direct the questioner to email help@company.com f
+"I'm sorry, I don't know the answer to that." and then direct the questioner to contact help@company.com f
 or human assitance. Don't try to make up an answer. Always speak as if you were chatting to a friend. 
 Keep the conversations short to medium.
 context: {context}
