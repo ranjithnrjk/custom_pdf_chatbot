f560bcfe3d7971b17d069452a0b8e65f01b59666
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-02 09:37:26 +0530

    Modified views.py line: 61 ==> chatbot functions wiht multiple chains instead of single chain

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,102 @@
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from langchain.vectorstores import Chroma
+# from langchain.embeddings import SentenceTransformerEmbeddings
+# from langchain.prompts import PromptTemplate
+# from langchain.schema import StrOutputParser
+# from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
+# from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM 
+# from transformers import pipeline
+
+# import torch
+
+# device = torch.device('cpu')
+# checkpoint = "MBZUAI/LaMini-T5-738M"
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# base_model = AutoModelForSeq2SeqLM.from_pretrained(
+#     checkpoint,
+#     device_map=device,
+#     torch_dtype=torch.float32
+# )
+
+# pipe = pipeline(
+#         'text2text-generation',
+#         model = base_model,
+#         tokenizer = tokenizer,
+#         max_length = 256,
+#         do_sample = True,
+#         temperature = 0.01,
+#     )
+# model = HuggingFacePipeline(pipeline=pipe)
+
+
+# # Set up prompts for standalone question, retriever, and answer templates
+# standaloneQuestionTemplate = "Given a question, convert it to a standalone question. question: {question} standalone question: "
+# standaloneQuestionPrompt = PromptTemplate.from_template(standaloneQuestionTemplate)
+
+
+# persist_directory = './db'
+# embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2") # create the open-source embedding function
+# vectordb = Chroma(persist_directory=persist_directory, # Now we can load the persisted database from disk, and use it as normal.  
+#                   embedding_function=embeddings)
+# retriever = vectordb.as_retriever()
+
+# def combine_documents(docs):
+#     return '\n'.join(doc['pageContent'] for doc in docs)
+
+# def retrieve_context(resp):
+#     return resp.get("standalone_question")
+
+
+# answerTemplate = '''You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend. Keep the conversations short to medium.
+# context: {context}
+# question: {question}
+# answer:
+# '''
+# answerPrompt = PromptTemplate.from_template(answerTemplate)
+
+# # CHAINS to combine the above created prompts
+# standaloneChain = standaloneQuestionPrompt | model | StrOutputParser()
+# retrieverChain = RunnableLambda(retrieve_context) | retriever | RunnableLambda(combine_documents)
+# answerChain = answerPrompt | model | StrOutputParser()
+
+# chain = ({
+#     "standalone_question": standaloneChain,
+#     "original_input": RunnablePassthrough()
+# } | 
+# {
+#     "context": retrieverChain,
+#     "question": lambda original_input: original_input.question,
+# } |
+# answerChain
+# )
+
+
+# # Create your views here.
+# def chatbot(request):
+#     if request.method == 'POST':
+#         message = request.POST.get('message')
+
+#         # Do something with the message here using LLM
+#         response = chain.invoke({'question': message})
+
+#         return JsonResponse({'response': response})
+#     return render(request, 'chatbot.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from langchain.vectorstores import Chroma
@@ -14,7 +113,7 @@ import torch
 
 device = torch.device('cpu')
 checkpoint = "MBZUAI/LaMini-T5-738M"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, truncation=True, max_length=512)
 base_model = AutoModelForSeq2SeqLM.from_pretrained(
     checkpoint,
     device_map=device,
@@ -25,7 +124,7 @@ pipe = pipeline(
         'text2text-generation',
         model = base_model,
         tokenizer = tokenizer,
-        max_length = 256,
+        max_length = 1024,
         do_sample = True,
         temperature = 0.01,
     )
@@ -46,11 +145,11 @@ retriever = vectordb.as_retriever()
 def combine_documents(docs):
     return '\n'.join(doc['pageContent'] for doc in docs)
 
-def retrieve_context(resp):
+def retrieve_standalone_question(resp):
     return resp.get("standalone_question")
 
 
-answerTemplate = '''You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend. Keep the conversations short to medium.
+answerTemplate = '''You are a helpful and enthusiastic support bot who can answer a given question based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." and direct the questioner to email help@company.com for human assitance. Don't try to make up an answer. Always speak as if you were chatting to a friend. Keep the conversations short to medium.
 context: {context}
 question: {question}
 answer:
@@ -59,20 +158,23 @@ answerPrompt = PromptTemplate.from_template(answerTemplate)
 
 # CHAINS to combine the above created prompts
 standaloneChain = standaloneQuestionPrompt | model | StrOutputParser()
-retrieverChain = RunnableLambda(retrieve_context) | retriever | RunnableLambda(combine_documents)
+retrieverChain = RunnableLambda(retrieve_standalone_question) | retriever | RunnableLambda(combine_documents)
 answerChain = answerPrompt | model | StrOutputParser()
 
-chain = ({
-    "standalone_question": standaloneChain,
-    "original_input": RunnablePassthrough()
-} | 
-{
-    "context": retrieverChain,
-    "question": lambda original_input: original_input.question,
-} |
-answerChain
-)
+# chain = ({
+#     "standalone_question": standaloneChain,
+#     "original_input": RunnablePassthrough()
+# } | 
+# {
+#     "context": retrieverChain,
+#     "question": lambda original_input: original_input.question,
+# } 
+# | answerChain
+# )
 
+# message = 'Who are you, and what do you do?, and who am I?'
+# response = chain.invoke({'question': message})
+# print(response)
 
 # Create your views here.
 def chatbot(request):
@@ -80,7 +182,13 @@ def chatbot(request):
         message = request.POST.get('message')
 
         # Do something with the message here using LLM
-        response = chain.invoke({'question': message})
+        context = ({
+            "standalone_question": standaloneChain,
+            "original_input": RunnablePassthrough()
+        } | retrieverChain )
+        retriever_response = context.invoke({'question': message})
+        ai_message = answerChain.invoke({'context': retriever_response[:512], 'question': message})
 
-        return JsonResponse({'response': response})
+
+        return JsonResponse({'response': ai_message})
     return render(request, 'chatbot.html')
