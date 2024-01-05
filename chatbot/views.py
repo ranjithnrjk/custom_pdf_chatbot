5ebd0320399f2b1a04188a6de3b742ae45fe32b2
ranjithkumarneeruganti <ranjithkumarneeruganti@gmail.com>
2024-01-05 18:31:14 +0530

    Changed the model

--- chatbot/views.py
+++ chatbot/views.py
@@ -19,7 +19,7 @@ unique_id = str(uuid.uuid4())
 
 device = torch.device('cpu')
 # checkpoint = "MBZUAI/LaMini-GPT-1.5B" 
-checkpoint = 'meta-llama/Llama-2-7b-chat-hf'
+checkpoint = 'Salesforce/codet5p-16b'
 # checkpoint = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 # checkpoint = "MBZUAI/LaMini-Cerebras-1.3B"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint, truncation=True, max_length=512, use_auth_token=True)
